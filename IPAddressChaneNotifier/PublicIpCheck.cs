c6463786c6f32f4cf4165c7e8112e7fd72da5a17
RedSpiderMkV <redspidermkv@hotmail.com>
2014-09-11 23:06:25 +0100

    Moving ip checking and logic to new class in prep for UI/service etc

---
+++ IPAddressChaneNotifier/PublicIpCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace IPAddressChaneNotifier
+{
+    internal class PublicIpCheck
+    {
+        public string ExternalIpAddress { get; private set; }
+
+        public bool GetExternalIpAddress()
+        {
+            try
+            {
+                ExternalIpAddress = new WebClient().DownloadString(ipCheckUrl);
+                return true;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Error retrieving IP address from url " + ipCheckUrl);
+            }
+        }
+
+        private const string ipCheckUrl = "http://www.binaryworld.webspace.virginmedia.com/Content/tools/ipcheck.php";
+    }
+}
